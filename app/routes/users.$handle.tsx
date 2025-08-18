e8e1c0028edb3f99ca7262d7779b1a0464b151cc
anass-lahlal <anass.lahlal@gmail.com>
2025-08-18 13:34:47 +0100

    feat: fetch user data

--- app/routes/users.$handle.tsx
+++ app/routes/users.$handle.tsx
@@ -1,4 +1,7 @@
+import {Image} from '@shopify/hydrogen';
+import {Link, useLoaderData} from 'react-router';
 import {LoaderFunctionArgs, type MetaFunction} from 'react-router';
+import {useVariantUrl} from '~/lib/variants';
 
 export const meta: MetaFunction<typeof loader> = () => {
   return [{title: `Hydrogen | Users`}];
@@ -15,9 +18,62 @@ async function loadCriticalData({
   request,
   params,
 }: LoaderFunctionArgs) {
-  return {};
+  const {handle} = params;
+  const {storefront} = context;
+
+  const {user} = await storefront.query(USER_METAOBJECT, {
+    variables: {handle: {handle: handle!, type: 'users'}},
+  });
+
+  return {user};
 }
 
 export default function User() {
+  const {user} = useLoaderData<typeof loader>();
   return <></>;
 }
+
+const USER_METAOBJECT = `#graphql
+  fragment ImageDetails on Image {
+    id
+    url
+    altText
+    width
+    height
+  }
+
+  query UserData(
+    $handle: MetaobjectHandleInput!
+  ) {
+        user: metaobject(handle: $handle) {
+            firstName: field(key: "first_name") {
+                value
+            }
+            image: field(key: "profile_image") {
+                ... on MetaobjectField {
+                    reference {
+                        ... on MediaImage {
+                            image {
+                                ... ImageDetails
+                            }
+                        }
+                    }
+                }
+            }
+            product: field(key: "assigned_product") {
+                ... on MetaobjectField {
+                    reference {
+                        ... on Product {
+                            title
+                            description
+                            handle
+                            featuredImage {
+                                ... ImageDetails
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+` as const;
